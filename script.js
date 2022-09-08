792abc2004a9a18dc52e17a5ec42fdedc7dcad5f
tercera Entrega arrays <shamelart.ca@gmail>
2022-09-07 23:44:54 -0300

    Primera Entrega Proyecto

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// declaracion array y objetos
 const malbec = [];
 const cabernet = [];
 
@@ -44,58 +43,59 @@ switch (tipoBebida) {
             inventChacabMalbec = parseInt(prompt("Cantidad Botellas en existencia: "))
             if (inventChacabMalbec <= malbec[0].minimo) {
                 malbec[0].cantidad = inventChacabMalbec;
-                alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
-                pregunta = prompt("Se realizó alguna compra de este producto s/n?");
-                if (pregunta == "s") {
-                    adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
-                    malbec[0].cantidad = malbec[0].cantidad + adicionar;
-                    alert("Cantidad de Botella Chacabuco Malbe es " + malbec[0].cantidad)
-                } else alert("debe realizar compra del producto Chacabuco Malbec para generar stoke minimo");
+                    alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
+                    pregunta = prompt("Se realizó alguna compra de este producto s/n?");
+                        if (pregunta == "s") {
+                            adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
+                                malbec[0].cantidad = malbec[0].cantidad + adicionar;
+                                alert("Cantidad de Botella Chacabuco Malbe es " + malbec[0].cantidad);
+                        } else alert("debe realizar compra del producto Chacabuco Malbec para generar stoke minimo");
             } 
             else {
                 malbec[0].cantidad = inventChacabMalbec;
-                alert ("Cantidad de Botella Chacabuco Malbec es " + malbec[0].cantidad );
+                    alert ("Cantidad de Botella Chacabuco Malbec es " + malbec[0].cantidad );
             }    
             
         } 
         else if (nombProducto == 2) {
-            inventConeVerde = parseInt(prompt("Cantidad Botellas en existencia: "))
-            if (inventConeVerde <= malbec[1].minimo) {
-                malbec[1].cantidad = inventConeVerde;
-                alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
-                pregunta = prompt("Se realizó alguna compra de este producto s/n?");
-                if (pregunta == "s") {
-                    adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
-                    malbec[1].cantidad = malbec[1].cantidad + adicionar;
-                    alert("Cantidad de Botella Conejo Verde Malbe es " + malbec[1].cantidad)
-                } else alert("debe realizar compra del producto Conejo Verde Malbec para generar stoke minimo");
-            } else {
-                malbec[1].cantidad = inventConeVerde;
-                alert ("Cantidad de Botella Conejo Verde Malbec es " + malbec[1].cantidad );
-            } 
+            inventConeVerde = parseInt(prompt("Cantidad Botellas en existencia: "));
+                if (inventConeVerde <= malbec[1].minimo) {
+                    malbec[1].cantidad = inventConeVerde;
+                        alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
+                        pregunta = prompt("Se realizó alguna compra de este producto s/n?");
+                            if (pregunta == "s") {
+                                adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
+                                    malbec[1].cantidad = malbec[1].cantidad + adicionar;
+                                    alert("Cantidad de Botella Conejo Verde Malbe es " + malbec[1].cantidad);
+                            } else alert("debe realizar compra del producto Conejo Verde Malbec para generar stoke minimo");
+                } else {
+                     malbec[1].cantidad = inventConeVerde;
+                         alert ("Cantidad de Botella Conejo Verde Malbec es " + malbec[1].cantidad );
+                } 
             break; 
         }      
         
     case 2:
        
-        inventChacabCabernet = parseInt(prompt("Cantidad Botellas en existencia: "))
+        inventChacabCabernet = parseInt(prompt("Cantidad Botellas en existencia: "));
         if (inventChacabCabernet <= malbec[0].minimo) {
             cabernet[0].cantidad = inventChacabCabernet;
-            alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
-            pregunta = prompt("Se realizó alguna compra de este producto s/n?");
-            if (pregunta == "s") {
-                adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
-                cabernet[0].cantidad = cabernet[0].cantidad + adicionar;
-                alert("Cantidad de Botella Chacabuco Malbe es " + cabernet[0].cantidad);
-            }
-            else{
-                alert("debe realizar compra del producto Chacabuco Malbec para generar stoke minimo");                    
-            } 
+                alert("El producto esta bajo de stock requerido, se recomienda hacer compra");
+                pregunta = prompt("Se realizó alguna compra de este producto s/n?");
+                    if (pregunta == "s") {
+                        adicionar = parseInt(prompt("Ingresar cantidad comprada:"));
+                        cabernet[0].cantidad = cabernet[0].cantidad + adicionar;
+                            alert("Cantidad de Botella Chacabuco Malbe es " + cabernet[0].cantidad);
+                    }
+                    else{
+                        alert("debe realizar compra del producto Chacabuco Malbec para generar stoke minimo");                    
+                    } 
         }   
         else {
             cabernet[0].cantidad = inventChacabCabernet;
-            alert ("Cantidad de Botella Chacabuco Malbec es " + cabernet[0].cantidad );
+                alert ("Cantidad de Botella Chacabuco Malbec es " + cabernet[0].cantidad );
         } 
+
         break;
     
     default:
